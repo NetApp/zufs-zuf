bd92d7f3d66ce8ed14b1243dbf581b41488ce0fd
Boaz Harrosh <boazh@netapp.com>
2018-02-28 16:30:44 +0200

    zus_api: Remove the statx definitions
    Once we are on 4.15 we have it in the Kernel
    
    Boaz

--- fs/zuf/zus_api.h
+++ fs/zuf/zus_api.h
@@ -20,20 +20,8 @@
 #include <stddef.h>
 #include <asm/statfs.h>
 
-/* TODO: #include <linux/stat.h>
- * When these apear in newer kernel version */
-#define STATX_TYPE		0x00000001U	/* Want/got stx_mode & S_IFMT */
-#define STATX_MODE		0x00000002U	/* Want/got stx_mode & ~S_IFMT */
-#define STATX_NLINK		0x00000004U	/* Want/got stx_nlink */
-#define STATX_UID		0x00000008U	/* Want/got stx_uid */
-#define STATX_GID		0x00000010U	/* Want/got stx_gid */
-#define STATX_ATIME		0x00000020U	/* Want/got stx_atime */
-#define STATX_MTIME		0x00000040U	/* Want/got stx_mtime */
-#define STATX_CTIME		0x00000080U	/* Want/got stx_ctime */
-#define STATX_INO		0x00000100U	/* Want/got stx_ino */
-#define STATX_SIZE		0x00000200U	/* Want/got stx_size */
-#define STATX_BLOCKS		0x00000400U	/* Want/got stx_blocks */
-#define STATX_BASIC_STATS	0x000007ffU	/* The stuff in the normal stat struct */
+#include <linux/stat.h>
+
 #define ZUFS_STATX_VERSION	0x00000800U	/* someone forgot ? */
 
 /* Version stuff (stored at s_version):
