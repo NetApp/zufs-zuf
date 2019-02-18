de4bb68fe5980931499fbe0e3391e885af62a0b9
Boaz Harrosh <boazh@netapp.com>
2019-02-18 07:41:38 +0200

    zuf: Namei and directory operations
    Introducing Creation/deletion of files
    Directory add/remove
    Other namei operations
    
    This is all a very STD Kernel way of doing things.
    Each VFS operation is packed and dispatched to Server.
    After dispatch return, pushing results into Kernel
    structures
    
    NOTE: The use of a zufs_inode communication structure
    that is returned as a zufs_dpp_t (Dual port pointer)
    Both Kernel and Server can read/write to this object.
    If Kernel modifies this object it is always before
    the dispatch so server can persist the changes.
    It is also used by Server to return new info to be updated
    into the vfs_inode.
    In a pmem system this object can be directly pointing
    to storage.
    
    Signed-off-by: Boaz Harrosh <boazh@netapp.com>

---
+++ fs/zuf/file.c
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * BRIEF DESCRIPTION
+ *
+ * File operations for files.
+ *
+ * Copyright (c) 2018 NetApp Inc. All rights reserved.
+ *
+ * ZUFS-License: GPL-2.0. See module.c for LICENSE details.
+ *
+ * Authors:
+ *	Boaz Harrosh <boazh@netapp.com>
+ *	Sagi Manole <sagim@netapp.com>"
+ */
+
+#include "zuf.h"
+
+long __zuf_fallocate(struct inode *inode, int mode, loff_t offset, loff_t len)
+{
+	return -ENOTSUPP;
+}
+
+ssize_t zuf_read_iter(struct kiocb *kiocb, struct iov_iter *ii)
+{
+	return -ENOTSUPP;
+}
+
+ssize_t zuf_write_iter(struct kiocb *kiocb, struct iov_iter *ii)
+{
+	return -ENOTSUPP;
+}
+
+const struct file_operations zuf_file_operations = {
+	.open			= generic_file_open,
+};
+
+const struct inode_operations zuf_file_inode_operations = {
+	.setattr	= zuf_setattr,
+	.getattr	= zuf_getattr,
+	.update_time	= zuf_update_time,
+};
